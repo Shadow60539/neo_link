a448d47ba899da1cb31d7761100345f26974a80e
Shadow60539 <sanjeev.181ch014@nitk.edu.in>
2020-08-04 16:28:44 +0530

    modified settings page

--- lib/ui/pages/settings_page.dart
+++ lib/ui/pages/settings_page.dart
@@ -1,232 +1,323 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:neo_link_app/ui/colors/colors.dart';
-import 'package:neo_link_app/ui/pages/guardian_page.dart';
 import 'package:neo_link_app/ui/widgets/app_bar.dart';
 
-class SettingsPage extends StatelessWidget {
+enum bioSwitch { active, inactive }
+enum devSwitch { active, inactive }
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  final ValueNotifier<bioSwitch> _bio =
+      ValueNotifier<bioSwitch>(bioSwitch.active);
+  final ValueNotifier<devSwitch> _dev =
+      ValueNotifier<devSwitch>(devSwitch.active);
   @override
   Widget build(BuildContext context) {
     final style = Theme.of(context).textTheme.bodyText1;
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Container(
-              color: kDarkBlue,
+    return AnimatedBuilder(
+      animation: Listenable.merge([_bio, _dev]),
+      builder: (BuildContext context, Widget child) {
+        return SafeArea(
+          child: Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(50),
+              child: MyAppBar(),
             ),
-            _backgroundContainer(style, context),
-            _draggableSheet(style),
-          ],
-        ),
-      ),
+            backgroundColor: Colors.white,
+            body: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                _backgroundContainer(style, context),
+                SizedBox(
+                  height: 20,
+                ),
+                _draggableSheet(style, context),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
   Widget _backgroundContainer(TextStyle style, BuildContext context) {
     return Column(
       children: <Widget>[
-        MyAppBar(),
-        GestureDetector(
-          onTap: () => Navigator.push(
-              context, MaterialPageRoute(builder: (context) => GuardianPage())),
-          child: Container(
-            child: Column(
+        Column(
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.center,
               children: <Widget>[
                 Container(
-                  height: 60,
-                  width: 60,
+                  height: 90,
+                  width: 90,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.lightBlue),
+                ),
+                Container(
+                  height: 80,
+                  width: 80,
                   decoration: BoxDecoration(
                       shape: BoxShape.circle, color: Colors.white),
-                  child: Image.asset(
-                    'assets/user_dp.png',
-                    scale: 1.6,
-                  ),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                Text(
-                  'Shakunthala',
-                  style:
-                      style.copyWith(fontSize: 16, fontWeight: FontWeight.bold),
+                  child: Image.asset('assets/user_dp.png'),
                 ),
               ],
             ),
-          ),
+            SizedBox(
+              height: 11,
+            ),
+            Text(
+              'Guardian Name',
+              style: style.copyWith(fontSize: 24, color: Colors.black),
+            ),
+          ],
         ),
       ],
     );
   }
 
-  Widget _draggableSheet(TextStyle style) {
-    return DraggableScrollableSheet(
-      builder: (context, c) {
-        return Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30), topRight: Radius.circular(30))),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 40, left: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+  Widget _draggableSheet(TextStyle style, context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              spreadRadius: 0.5,
+              blurRadius: 20,
+            )
+          ],
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.only(bottom: 20),
+          child: Column(
+            children: <Widget>[
+              _content(style, context),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Developed By',
+                style: style.copyWith(color: Colors.black54, fontSize: 12),
+              ),
+              Text(
+                'LibreHeath',
+                style: style.copyWith(color: Colors.black54, fontSize: 12),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _content(TextStyle style, context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 30, left: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.fingerprint,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: kDividerColor))),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Image.asset('assets/fingerprint.png'),
-                          SizedBox(
-                            width: 3,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(color: kDividerColor))),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Text(
-                                  'Biometric Authentication',
-                                  style: style.copyWith(
-                                      fontSize: 16, color: Colors.black),
-                                ),
-                                Switch(value: true, onChanged: (v) {})
-                              ],
-                            ),
-                          ),
-                        ],
+                      Text(
+                        'Biometric Authentication',
+                        style:
+                            style.copyWith(fontSize: 16, color: Colors.black),
                       ),
-                      SizedBox(
-                        height: 26,
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Image.asset('assets/password.png'),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(bottom: 20),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(color: kDividerColor))),
-                            child: Text(
-                              'Passcode',
-                              style: style.copyWith(
-                                  fontSize: 16, color: Colors.black),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 26,
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Image.asset('assets/chat.png'),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(bottom: 20),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(color: kDividerColor))),
-                            child: Text(
-                              'Chat with us',
-                              style: style.copyWith(
-                                  fontSize: 16, color: Colors.black),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 26,
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Image.asset('assets/about.png'),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(bottom: 20),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(color: kDividerColor))),
-                            child: Text(
-                              'About',
-                              style: style.copyWith(
-                                  fontSize: 16, color: Colors.black),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 26,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Image.asset('assets/developer_mode.png'),
-                          SizedBox(
-                            width: 3,
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(right: 48),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(color: kDividerColor))),
-                            child: Row(
-                              children: <Widget>[
-                                Text(
-                                  'Developer Mode',
-                                  style: style.copyWith(
-                                      fontSize: 16, color: Colors.black),
-                                ),
-                                Switch(value: true, onChanged: (v) {})
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                      Switch(
+                        value: _bio.value == bioSwitch.active,
+                        onChanged: (v) {
+                          if (v)
+                            _bio.value = bioSwitch.active;
+                          else
+                            _bio.value = bioSwitch.inactive;
+                        },
+                        activeColor: Colors.red,
+                      )
                     ],
                   ),
                 ),
-                SizedBox(
-                  height: 166,
-                ),
-                Text(
-                  'Developed By',
-                  style: style.copyWith(color: Colors.grey, fontSize: 12),
-                ),
-                SizedBox(
-                  height: 11,
-                ),
-                Text(
-                  'LibreHeath',
-                  style: style.copyWith(color: Colors.black54, fontSize: 12),
-                ),
-                SizedBox(
-                  height: 15,
-                )
-              ],
-            ),
-            controller: c,
+              ),
+            ],
           ),
-        );
-      },
-      initialChildSize: 0.7,
-      maxChildSize: 0.7,
-      minChildSize: 0.6,
+          SizedBox(
+            height: 26,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.vpn_key,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: kDividerColor))),
+                  child: Text(
+                    'Passcode',
+                    style: style.copyWith(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 26,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.chat,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: kDividerColor))),
+                  child: Text(
+                    'Chat with us',
+                    style: style.copyWith(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 26,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.error,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: kDividerColor))),
+                  child: Text(
+                    'About',
+                    style: style.copyWith(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 26,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(
+                Icons.developer_mode,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: kDividerColor))),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Developer Mode',
+                        style:
+                            style.copyWith(fontSize: 16, color: Colors.black),
+                      ),
+                      Switch(
+                        value: _dev.value == devSwitch.active,
+                        onChanged: (v) {
+                          if (v)
+                            _dev.value = devSwitch.active;
+                          else
+                            _dev.value = devSwitch.inactive;
+                        },
+                        activeColor: Colors.red,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 26,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.help,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: kDividerColor))),
+                  child: Text(
+                    'Need Help ?',
+                    style: style.copyWith(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
